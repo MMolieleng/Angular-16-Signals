e4885437ac240bb27b83febd90edff9deacad9d6
MMolieleng <mjmolieleng@gmail.com>
2023-07-23 16:08:04 +0200

    added logic using signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed, signal } from '@angular/core';
+import { BlogService } from './services/blog.service';
+import { BlogPost } from './models/BlogPost';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  posts: Signal<BlogPost[]> = computed(() => {
+    return this.blogService.posts() as BlogPost[];
+  });
+
+  selectedPost = signal<BlogPost | null>(null);
+
+
   title = 'ng-blog';
+
+  constructor(private blogService: BlogService) {
+    blogService.getPosts();
+  }
+
+  setSelectedPost(post: BlogPost) {
+    console.log({ post })
+    this.selectedPost.set(post);
+  }
+
+
+  // numberOfPosts = computed(() => this.posts().length);
+
+  // eff = effect(() => {
+  //   console.log(this.posts());
+  // });
+
 }
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { BlogPost } from '../models/BlogPost';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  posts = signal<BlogPost[]>([]);
-  constructor() { }
+
+  public posts = signal<BlogPost[]>([]);
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getPosts() {
+    this.http.get<BlogPost[]>('https://jsonplaceholder.typicode.com/posts').subscribe({
+      next: data => {
+        this.posts.set(data);
+      },
+      error: error => {
+        console.error(error);
+      }
+    });
+  }
+
+  public getPost(id: number) {
+    return this.posts().find(post => post.id === id);
+  }
+
+  public addPost(post: BlogPost) {
+    this.posts().push(post);
+  }
+
+  deletePost(id: number) {
+    this.posts().splice(id, 1);
+  }
 }
